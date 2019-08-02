b6cd8ea0195ad3ad32da68d171b5e3f3bc93d53b
initialA <azmim2011@gmail.com>
2019-08-02 15:35:52 +0800

    deleted:    motd/banner.sh 	deleted:    pythonlearn/factorial.py 	deleted:    pythonlearn/loopbyrange.py 	deleted:    pythonlearn/masukkaninput.py 	deleted:    pythonlearn/speedtest.csv 	deleted:    pythonlearn/split.py 	deleted:    pythonlearn/writetocsv.py 	deleted:    waktusolat/waktusolat.py 	deleted:    who/whois.py

--- pythonlearn/masukkaninput.py
+++
@@ -1,8 +0,0 @@
-# masukkan input
-print('Hello!, this is my python learning area')
-
-name = input('What is your name?\n')
-print('Hello, my name is %s.' % name)
-
-age = input('What is your age?\n')
-print('My age is %s.'% age)
--- who/whois.py
+++
@@ -1,9 +0,0 @@
-# import whois
-from whois import whois
-
-# masukkan input
-data = input("Masukkan Domain Untuk WHOIS: ")
-whoisLookup = whois(data)
-
-# Result WHOIS
-print(whoisLookup.text)
